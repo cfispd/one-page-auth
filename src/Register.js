2ef7005dae7b85a5a6cfb598570377c39ee51546
杨无为 <yangwuwei@yangwuweis-MacBook-Pro.local>
2023-09-11 12:58:31 -0700

    first commit

--- src/Register.js
+++ src/Register.js
@@ -1,4 +1,4 @@
-import React, { useSyncExternalStore } from 'react'
+import React from 'react'
 import { Form, Button } from "react-bootstrap";
 import { useState } from 'react';
 import axios from "axios";
